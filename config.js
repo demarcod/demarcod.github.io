e29f29a3672ed56e6747bd7bb64a06ffdd4e002f
Donnatella DeMarco <donnatella.maria@gmail.com>
2020-05-14 02:15:02 -0400

    added backend to email form on contact page

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    USER: 'donnatella.maria@gmail.com',
+    PASS: 'do you really think i\'d post my email password on the internet?'
+}
--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -3,6 +3,8 @@ import Hero from '../Components/Hero';
 import Content from '../Components/Content';
 import Form from 'react-bootstrap/form';
 import Button from 'react-bootstrap/Button';
+import Axios from 'axios';
+
 
 class Contact extends React.Component{
 
@@ -11,42 +13,71 @@ class Contact extends React.Component{
         this.state = {
             name: '',
             email: '',
-            message: '',
-            disabled: false,
-            emailSent: null,
+            message: ''
         }
     }
+
+    handleSubmit(e){
+        e.preventDefault();
+        Axios({
+            method: "POST",
+            url: "http://localhost:3030/send",
+            data: this.state
+        }).then((response)=>{
+            if (response.data.status === 'success'){
+                alert("Message Sent.");
+                this.resetForm()
+            } else if(response.data.status === 'fail'){
+                alert("Message failed to send.")
+            }
+        })
+    }
+
+    resetForm(){
+        this.setState({name: '', email: '', message: ''})
+    }
+    
     render() {
         return(
             <div>
                 <Hero title={this.props.title} />
 
                 <Content>
-                    <Form>
+                    <Form onSubmit={this.handleSubmit.bind(this)} method="POST">
                         <Form.Group>
                             <Form.Label htmlFor="full-name">Full Name</Form.Label>
-                            <Form.Control id="full-name" name="name" type="text" value={this.state.name} onChange={this.handleChange}></Form.Control>
+                            <Form.Control id="full-name" name="name" type="text" value={this.state.name} onChange={this.onNameChange.bind(this)}></Form.Control>
                         </Form.Group>
                         <Form.Group>
                             <Form.Label htmlFor="email">Email</Form.Label>
-                            <Form.Control id="email" name="email" type="email" value={this.state.email} onChange={this.handleChange}></Form.Control>
+                            <Form.Control id="email" name="email" type="email" value={this.state.email} onChange={this.onEmailChange.bind(this)}></Form.Control>
                         </Form.Group>
                         <Form.Group>
                             <Form.Label htmlFor="message">Message</Form.Label>
-                            <Form.Control id="message" name="message" as="textarea" row="3" value={this.state.message} onChange={this.handleChange}></Form.Control>
+                            <Form.Control id="message" name="message" as="textarea" rows="5" value={this.state.message} onChange={this.onMessageChange.bind(this)}></Form.Control>
                         </Form.Group>
 
-                        <Button className="d-inline-block" variant="primary" type="submit" disabled={this.state.disabled}>
+                        <Button className="d-inline-block" variant="primary" type="submit">
                             Send
                         </Button>
 
-                        {this.state.emailSent === true && <p className="d-inline success-msg">Email Sent</p>}
-                        {this.state.emailSent === false && <p className="d-inline err-msg">Email Not Sent</p>}
 
                     </Form>
                 </Content>
             </div>
         );
+    }
+
+    onNameChange(event) {
+        this.setState({name: event.target.value})
+    }
+
+    onEmailChange(event) {
+        this.setState({email: event.target.value})
+    }
+
+    onMessageChange(event) {
+        this.setState({message: event.target.value})
     }
 }
 
