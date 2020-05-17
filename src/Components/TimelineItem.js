24a6cc4279775297b1796f54c0c6edc876371794
Donnatella DeMarco <donnatella.maria@gmail.com>
2020-05-17 16:36:39 -0400

    added more styling to timeline component

--- src/Components/TimelineItem.js
+++ src/Components/TimelineItem.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
-const TimelineItem = ({ data}) => (
+const TimelineItem = ({data}) => (
     <div className="timeline-item">
         <div className="timeline-item-content">
             <span className="tag" style={{ background: data.category.color }}>
                 {data.category.tag}
             </span>
             <time>{data.date}</time>
-            <p>{data.text}</p>
+            <p className="timeline-title">{data.title}</p>
+            <p className="timeline-bullet-point">{data.text}</p>
             {data.link && (
             <a href={data.link.url} target="_blank" rel="noopener noreferrer">
                 {data.link.text}
--- src/data.js
+++ src/data.js
@@ -1,5 +1,6 @@
 const data =[
 {
+    title: 'CIDC Business Website - Full Stack Developer',
     text: 'CIDC Business Website - Full Stack Developer',
     date: 'December 2019 - Now',
     category: {
@@ -12,15 +13,19 @@ const data =[
     }
 },
 {
-    text: 'University of Windsor - Full-Time Student',
+    title: 'University of Windsor - Full-Time Student',
+    text: '•CIDC Business',
     date: 'September 2015 - October 2019',
     category: {
-        tag: 'Studies',
+        tag: 'Education',
         color: '#FFDB14'
     }
 },
 {
-    text: 'Statistics Canada - Full-Stack Developer',
+    title: 'Statistics Canada - Full-Stack Developer',
+    text: `•  Developed forms using a .NET framework, C#, XAML, and MVC architecture for a desktop application used by the Trade Division to update and modify values of Canada’s imports/exports
+•  Designed and updated unit tests to ensure correctness of various forms on a desktop application
+•  Created and modified databases, scripts in MySQL, and complex queries for data analysis by a team of 15 employees from the Trade Division`,
     date: 'January 2018 - August 2018',
     category: {
         tag: 'Co-op',
@@ -28,7 +33,8 @@ const data =[
     }
 },
 {
-    text: 'Pernod Ricard - Junior IT Specialist',
+    title: 'Pernod Ricard - Junior IT Specialist',
+    text: 'CIDC Business Website - Full Stack Developer',
     date: 'May 2017 - December 2017',
     category: {
         tag: 'Co-op/Part-Time',
