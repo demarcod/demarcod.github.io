9ae059d2010fb51812a5e165d0a98231db2da193
Donnatella DeMarco <donnatella.maria@gmail.com>
2020-07-12 15:36:35 -0400

    Updates

--- config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    USER: 'donnatella.maria@gmail.com',
-    PASS: 'do you really think i\'d post my email password on the internet?'
-}
---
+++ precache-manifest.4723427c312550c2d9aecaf2fec05bf5.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47af70ee6f44a50503c940dabd2e2dc0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7902b7f5a18186dc5889",
+    "url": "/static/css/2.c26e03e4.chunk.css"
+  },
+  {
+    "revision": "7fb190852f5d20de083f",
+    "url": "/static/css/main.65c1e805.chunk.css"
+  },
+  {
+    "revision": "7902b7f5a18186dc5889",
+    "url": "/static/js/2.1d589385.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.1d589385.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fb190852f5d20de083f",
+    "url": "/static/js/main.ddf21d51.chunk.js"
+  },
+  {
+    "revision": "cc1a23e0066835ad3a69",
+    "url": "/static/js/runtime-main.a2914572.js"
+  },
+  {
+    "revision": "b941549e0168ce8c686ee5d61755e980",
+    "url": "/static/media/avatar.b941549e.png"
+  },
+  {
+    "revision": "0ce9ac9e6163df73fbd5f531c1e952d3",
+    "url": "/static/media/full-avatar.0ce9ac9e.jpeg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
