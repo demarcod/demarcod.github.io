df608f52d5c8d2fbeeae0bbed9d2eaa42460c0c4
Donnatella DeMarco <donnatella.maria@gmail.com>
2020-08-12 18:21:43 -0400

    Updates

---
+++ precache-manifest.303de7eaeb9fe0e9c51c0319a5655540.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccfcde2fbff08f7e3dc90e99c269e2ff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0f02a5d4b2e2b4b4603",
+    "url": "/static/css/2.c26e03e4.chunk.css"
+  },
+  {
+    "revision": "4117d0f750f156241455",
+    "url": "/static/css/main.125e0b61.chunk.css"
+  },
+  {
+    "revision": "e0f02a5d4b2e2b4b4603",
+    "url": "/static/js/2.f5f7381f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.f5f7381f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4117d0f750f156241455",
+    "url": "/static/js/main.d2dab9f0.chunk.js"
+  },
+  {
+    "revision": "cc1a23e0066835ad3a69",
+    "url": "/static/js/runtime-main.a2914572.js"
+  },
+  {
+    "revision": "b941549e0168ce8c686ee5d61755e980",
+    "url": "/static/media/avatar.b941549e.png"
+  },
+  {
+    "revision": "0ce9ac9e6163df73fbd5f531c1e952d3",
+    "url": "/static/media/full-avatar.0ce9ac9e.jpeg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
