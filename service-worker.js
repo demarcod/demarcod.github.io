ead7a9657348d59daac1a8b6bd367e0ba1f0dfd2
Donnatella DeMarco <donnatella.maria@gmail.com>
2020-08-13 00:20:36 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.303de7eaeb9fe0e9c51c0319a5655540.js"
+  "/precache-manifest.8adc9598d0a5c6d0f76e4fb6287d8a54.js"
 );
 
 self.addEventListener('message', (event) => {
